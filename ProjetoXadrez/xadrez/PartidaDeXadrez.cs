891201cc5b4812bc65a892ae210fe9fba5125bbd
Pedro Matheus Coradini <matt_6.9.6@hotmail.com>
2021-11-11 19:40:00 -0300

    jogada especial roque pequeno e roque grande

--- ProjetoXadrez/xadrez/PartidaDeXadrez.cs
+++ ProjetoXadrez/xadrez/PartidaDeXadrez.cs
@@ -37,6 +37,29 @@ namespace Tabuleiro.xadrez
             {
                 capturadas.Add(pecaCapturada);
             }
+
+            //jogadaEspecial roque grande
+            if (p is Rei && destino.coluna == origem.coluna - 2)
+            {
+                Posicao origemT = new Posicao(origem.linha, origem.coluna - 4);
+                Posicao destinoT = new Posicao(origem.linha, origem.coluna - 1);
+                Peca T = tab.retirarPeca(origemT);
+                T.incrementarQuantidadeMovimentos();
+                tab.colocarPeca(T, destinoT);
+
+            }
+
+            //jogadaEspecial roque pequeno
+            if (p is Rei && destino.coluna == origem.coluna + 2)
+            {
+                Posicao origemT = new Posicao(origem.linha, origem.coluna + 3);
+                Posicao destinoT = new Posicao(origem.linha, origem.coluna + 1);
+                Peca T = tab.retirarPeca(origemT);
+                T.incrementarQuantidadeMovimentos();
+                tab.colocarPeca(T, destinoT);
+
+            }
+
             return pecaCapturada;
         }
 
@@ -49,6 +72,28 @@ namespace Tabuleiro.xadrez
                 tab.colocarPeca(pecaCapturada, destino);
                 capturadas.Remove(pecaCapturada);
             }
+
+            //jogadaEspecial roque pequeno
+            if (p is Rei && destino.coluna == origem.coluna + 2)
+            {
+                Posicao origemT = new Posicao(origem.linha, origem.coluna + 3);
+                Posicao destinoT = new Posicao(origem.linha, origem.coluna + 1);
+                Peca T = tab.retirarPeca(destinoT);
+                T.decrementarQuantidadeMovimentos();
+                tab.colocarPeca(T, origemT);
+            }
+
+            //jogadaEspecial roque grande
+            if (p is Rei && destino.coluna == origem.coluna - 2)
+            {
+                Posicao origemT = new Posicao(origem.linha, origem.coluna - 4);
+                Posicao destinoT = new Posicao(origem.linha, origem.coluna - 1);
+                Peca T = tab.retirarPeca(destinoT);
+                T.decrementarQuantidadeMovimentos();
+                tab.colocarPeca(T, origemT);
+
+            }
+
             tab.colocarPeca(p, origem);
         }
 
@@ -230,7 +275,7 @@ namespace Tabuleiro.xadrez
             colocarNovaPeca('b', 1, new Cavalo(tab, Cor.Branca));
             colocarNovaPeca('c', 1, new Bispo(tab, Cor.Branca));
             colocarNovaPeca('d', 1, new Dama(tab, Cor.Branca));
-            colocarNovaPeca('e', 1, new Rei(tab, Cor.Branca));
+            colocarNovaPeca('e', 1, new Rei(tab, Cor.Branca, this));
             colocarNovaPeca('f', 1, new Bispo(tab, Cor.Branca));
             colocarNovaPeca('g', 1, new Cavalo(tab, Cor.Branca));
             colocarNovaPeca('h', 1, new Torre(tab, Cor.Branca));
@@ -247,7 +292,7 @@ namespace Tabuleiro.xadrez
             colocarNovaPeca('b', 8, new Cavalo(tab, Cor.Preta));
             colocarNovaPeca('c', 8, new Bispo(tab, Cor.Preta));
             colocarNovaPeca('d', 8, new Dama(tab, Cor.Preta));
-            colocarNovaPeca('e', 8, new Rei(tab, Cor.Preta));
+            colocarNovaPeca('e', 8, new Rei(tab, Cor.Preta, this));
             colocarNovaPeca('f', 8, new Bispo(tab, Cor.Preta));
             colocarNovaPeca('g', 8, new Cavalo(tab, Cor.Preta));
             colocarNovaPeca('h', 8, new Torre(tab, Cor.Preta));
